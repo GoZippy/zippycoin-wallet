3b5550f0cc8af6a04d3bf59e3ce537c4d57dabaf
GoZippy <gotadvantage@gmail.com>
2025-06-30 12:24:13 -0500

    Fix @noble/hashes backend configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/globals.css'
+import { utils } from '@noble/hashes'
+
+// Configure @noble/hashes to use browser's Web Crypto API
+utils.setBackend(crypto)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
