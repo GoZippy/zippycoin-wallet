060c26c4edcf585f40b7d7079a40838bb07af49f
GoZippy <gotadvantage@gmail.com>
2025-06-30 12:30:08 -0500

    Fix @noble/hashes import error

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/globals.css'
-import { utils } from '@noble/hashes/utils'
+import { setBackend } from '@noble/hashes/utils'
 
 // Configure @noble/hashes to use browser's Web Crypto API
-utils.setBackend(crypto)
+setBackend(crypto)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
